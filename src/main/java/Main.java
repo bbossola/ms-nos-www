4845fe16f746310534fb1f624c2d557b2cdef1ee
Bruno Bossola <bbossola@gmail.com>
2015-03-05 21:39:21 +0000

    Fixed startup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
+import java.net.URL;
+import java.security.ProtectionDomain;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        Server server = new Server();
-        SelectChannelConnector connector = new SelectChannelConnector();
 
         int port = 4242;
         try {
@@ -16,16 +14,20 @@ public class Main {
         } catch (Exception ignore) {
         }
 
+        Server server = new Server();
+
+        SelectChannelConnector connector = new SelectChannelConnector();
         connector.setMaxIdleTime(60 * 60 * 1000);
         connector.setSoLingerTime(-1);
         connector.setPort(port);
-        server.addConnector(connector);
+        server.addConnector(connector); 
 
-        WebAppContext webAppContext = new WebAppContext();
-        webAppContext.setBaseResource(new ResourceCollection(
-                Resource.newResource("src/main/webapp"),
-                Resource.newClassPathResource("META-INF/resources")));
-        server.setHandler(webAppContext);
+        ProtectionDomain domain = Main.class.getProtectionDomain();
+        URL location = domain.getCodeSource().getLocation();
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setWar(location.toExternalForm());
+        server.setHandler(webapp);
 
         server.start();
         server.join();
